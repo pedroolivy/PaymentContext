ed54aefcdc429d6969b96d83c952e1c63ca4960d
Pedro <pedrooliveiradevs@gmail.com>
2025-04-20 15:14:48 -0300

    Testes dos ValueObjects

---
+++ PaymentContext.Tests/ValueObjects/DocumentTests.cs
@@ -0,0 +1,41 @@
+﻿using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests.ValueObjects
+{
+    [TestClass]
+    public class DocumentTests
+    {
+        [TestMethod]
+        public void ShouldReturnErrorWhenCNPJIsInvalid()
+        {
+            var doc = new Document("123", EDocumentType.CNPJ);
+            Assert.IsTrue(!doc.IsValid);
+        }
+
+        [TestMethod]
+        public void ShouldReturnSuccessWhenCNPJIsValid()
+        {
+            var doc = new Document("69650930000197", EDocumentType.CNPJ);
+            Assert.IsTrue(doc.IsValid);
+        }
+
+        [TestMethod]
+        public void ShouldReturnErrorWhenCPFIsInvalid()
+        {
+            var doc = new Document("123", EDocumentType.CPF);
+            Assert.IsTrue(!doc.IsValid);
+        }
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("26310984047")]
+        [DataRow("31018746021")]
+        [DataRow("90875537065")]
+        public void ShouldReturnSuccessWhenCPFIsValid(string cpf)
+        {
+            var doc = new Document(cpf, EDocumentType.CPF);
+            Assert.IsTrue(doc.IsValid);
+        }
+    }
+}
