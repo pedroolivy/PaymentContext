c80d217d1399c4c3c273fb362b29834a33a38043
Pedro <pedrooliveiradevs@gmail.com>
2025-04-20 17:43:24 -0300

    Adição de mais commands

--- PaymentContext.Domain/Handlers/SubscriptionHandler.cs
+++ PaymentContext.Domain/Handlers/SubscriptionHandler.cs
@@ -74,7 +74,58 @@ namespace PaymentContext.Domain.Handlers
                 return new CommandResult(false, "Não é possível realizar sua assinatura");
 
             _repository.CreateSubscription(student);
-            _emailService.Send(student.Name.ToString(), student.Email.Address, "bem vindo ao balta.io", "Sua assinatura foi criada");
+            _emailService.Send(student.Name.ToString(), student.Email.Address, "bem vindo!", "Sua assinatura foi criada");
+
+            return new CommandResult(true, "Assinatura realizada com sucesso");
+        }
+
+        public ICommandResult Handle(CreatePayPalSubscriptionCommand command)
+        {
+            if (_repository.DocumentExists(command.Document))
+                AddNotification("Document", "Este CPF já está em uso");
+
+            if (_repository.EmailExists(command.Email))
+                AddNotification("Email", "Este E-mail já está em uso");
+
+            var name = new Name(command.FirstName, command.LastName);
+            var document = new Document(command.Document, EDocumentType.CPF);
+            var email = new Email(command.Email);
+            var address = new Address(
+                command.Street, 
+                command.Number, 
+                command.Neighborhood, 
+                command.City, 
+                command.State,
+                command.Country, 
+                command.ZipCode);
+
+            var student = new Student(name, document, email);
+            var subscription = new Subscription(DateTime.Now.AddMonths(1));
+            var payment = new PayPalPayment(
+                command.TransactionCode,
+                command.PaidDate,
+                command.ExpireDate,
+                command.Total,
+                command.TotalPaid,
+                command.Payer,
+                new Document(
+                    command.PayerDocument, 
+                    command.PayerDocumentType),
+                address,
+                email
+            );
+
+            subscription.AddPayment(payment);
+            student.AddSubscription(subscription);
+
+            AddNotifications(name, document, email, address, student, subscription, payment);
+
+            if (!IsValid)
+                return new CommandResult(false, "Não é possível realizar sua assinatura");
+
+            _repository.CreateSubscription(student);
+
+            _emailService.Send(student.Name.ToString(), student.Email.Address, "bem vindo!", "Sua assinatura foi criada");
 
             return new CommandResult(true, "Assinatura realizada com sucesso");
         }
